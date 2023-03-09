8d729969850030ffdc5a0bf6d4e1c1357fcd2654
dleeftink <dennisleeftink@gmail.com>
2023-03-09 23:13:28 +1000

    1 file modified

--- api/hello.js
+++ api/hello.js
@@ -1,6 +1,8 @@
 const { select } = require('../src/utils');
 
-const { puppeteer } = require('puppeteer-core')
+const chromium = require('chrome-aws-lambda');
+const { puppeteer } = require('puppeteer-core');
+
 
 module.exports = async (req, res) => {
 
