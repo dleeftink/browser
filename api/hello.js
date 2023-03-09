da59b65e0ff2b053144670fc89b8805aa159966f
dleeftink <dennisleeftink@gmail.com>
2023-03-09 23:16:35 +1000

    1 file modified

--- api/hello.js
+++ api/hello.js
@@ -3,9 +3,21 @@ const { select } = require('../src/utils');
 const chromium = require('chrome-aws-lambda');
 const { puppeteer } = require('puppeteer-core');
 
-
 module.exports = async (req, res) => {
+  const executablePath = await chromium.executablePath;
 
-  res.send({ puppeteer });
+  const browser = await puppeteer.launch({
+    executablePath,
+    args: chromium.args,
+    headless: false,
+  });
 
+  const page = await browser.newPage();
+
+  await page.goto(
+    'https://data.stackexchange.com/stackoverflow/query/4038/find-interesting-unanswered-questions/'
+  );
+  const screenshot = await page.screenshot();
+
+  res.send({ screenshot });
 };
